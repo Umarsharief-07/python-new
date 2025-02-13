2ebde9604cd4374fac9c2655bf2dca404134c5e9
Umarsharief-07 <umarsharief21@gmail.com>
2025-02-13 11:49:51 +0530

    Update python.py

--- python.py
+++ python.py
@@ -1,6 +1,12 @@
-# app.py
+from flask import Flask
 
-# hello.py
-print("Hello, World!")
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+
+if __name__ == '__main__':
+    app.run(debug=True)
 
 
