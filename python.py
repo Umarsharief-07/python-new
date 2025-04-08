e0909d8e8aba8405ecf3e23a3e1f51b9caf3a1a5
Umarsharief-07 <umarsharief21@gmail.com>
2025-04-08 18:37:03 +0530

    Update python.py

--- python.py
+++ python.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route("/")
 
 def client1():
-    return "<h1>Welcome to Client-1 welcome checking </h1>"
+    return "<h1>Welcome to Client-1 welcome checking-1 </h1>"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
