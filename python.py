983f378c1ff986bb4b98c73c19da701324eb815d
Umarsharief-07 <umarsharief21@gmail.com>
2025-04-08 11:24:48 +0530

    Update python.py

--- python.py
+++ python.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def client2():
-    return "<h1>Welcome to Client-2 hai cdb</h1>"
+    return "<h1>Welcome to Client-2 hai csdb</h1>"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5001, debug=True)
