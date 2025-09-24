51ce0a9dfe47acf32873f18ff7828bfb778ddedf
Umar sharief <umarsharief21@gmail.com>
2025-09-24 12:46:15 +0530

    levelup

--- fronend.py
+++ fronend.py
@@ -6,6 +6,6 @@ app = Flask(__name__)
 def homepage():
     return "<h1>Welcome to the Frontend! Try <a href='/api'>/api</a> to hit the backend via ALB.</h1>"
 
-ac
+acauwfi
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
